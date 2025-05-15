26ba865ec95dce240841d993f98e9d4a34735242
Eduardo Luis de Siqueira <eduardoluis1295@gmail.com>
2025-05-15 20:41:40 -0300

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,16 @@
+  window.onscroll = function () {
+    navbRolagem();
+  };
+
+  function navbRolagem() {
+    const navbar = document.getElementById("navbar");
+    const logoText = document.getElementById("logoText");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      navbar.style.background = "rgba(0, 0, 0, 0.85)";
+      logoText.style.opacity = "0";
+    } else {
+      navbar.style.background = "transparent";
+      logoText.style.opacity = "1";
+    }
+  }
