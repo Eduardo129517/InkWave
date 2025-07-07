539d85913c386fe55c3d2ca39102cb1dd90343bd
Eduardo Luis de Siqueira <eduardoluis1295@gmail.com>
2025-07-07 18:28:20 -0300

    Add files via upload

--- scrpit.js
+++ scrpit.js
@@ -14,3 +14,31 @@ function navbRolagem() {
     logoText.style.opacity = "1";
   }
 }
+
+function bleach() {
+  window.open('sites-mangas/bleach-manga.html', '_self');
+}
+
+function jojolion() {
+  window.open('sites-mangas/jojolion-manga.html', '_self');
+}
+
+function devilman() {
+  window.open('sites-mangas/devilman-manga.html', '_self');
+}
+
+function centuryboys() {
+  window.open('sites-mangas/20th-century-boys-manga.html', '_self');
+}
+
+function pokemon() {
+  window.open('sites-mangas/pokemon-manga.html', '_self');
+}
+
+function berserk() {
+  window.open('sites-mangas/berserk-manga.html', '_self');
+}
+
+function bluelock() {
+  window.open('sites-mangas/blue-lock-manga.html', '_self');
+}
